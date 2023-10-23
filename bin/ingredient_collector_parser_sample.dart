42ca0b766a0fcf306ac3a403cab17631174b2513
Felix Schlegel <felix.schlegel@gmx.de>
2023-10-23 15:15:36 +0200

    feat: Added sample methods to use

--- bin/ingredient_collector_parser_sample.dart
+++ bin/ingredient_collector_parser_sample.dart
@@ -1,13 +1,34 @@
-import 'package:html/parser.dart';
+import 'package:html/dom.dart';
+import 'package:html/parser.dart' show parse;
 import 'package:http/http.dart' as http;
 
-var url = "https://example.org/";
+var urls = [
+  "https://example.org"
+];
 
 void main(List<String> arguments) async {
   var client = http.Client();
 
-  var response = await client.get(Uri.parse(url));
-  var document = parse(response.body);
+  for (var url in urls) {
+    var response = await client.get(Uri.parse(url));
+    var document = parse(response.body);
+
+    retrieveRecipeTitle(document);
+    retrieveRecipeServings(document);
+    retrieveRecipeIngredients(document);
+  }
 
   client.close();
+}
+
+void retrieveRecipeTitle(Document document) {
+  // TODO: Retrieve the recipe title from the document
+}
+
+void retrieveRecipeServings(Document document) {
+  // TODO: Retrieve the recipe servings from the document
+}
+
+void retrieveRecipeIngredients(Document document) {
+  // TODO: Retrieve the recipe ingredients from the document
 }
